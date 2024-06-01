96d71098c2e7263db26afcefc217eea0095e63c3
Cezarijus Kivylius <ltcezaris@gmail.com>
2024-06-01 11:31:04 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # webdeck-extension
-Web deck chrome extension to do even more!
+
+Web deck chrome extension to do even more! Install this via the chrome store to get more out of you plugins!
---
+++ background.js
@@ -0,0 +1,36 @@
+// Request listener
+console.log("hello from background.js");
+chrome.runtime.onMessageExternal.addListener(
+  async (request, sender, sendResponse) => {
+    console.log("Received message from", sender, request);
+    if (request) {
+      switch (request?.type) {
+        case "health":
+          sendResponse({
+            extension: true,
+            manifest: chrome.runtime.getManifest(),
+            runtime: chrome.runtime.id,
+          });
+          console.log(chrome);
+          break;
+        case "fetch":
+          const { method, url, body, options } = request;
+          try {
+            const resp = await fetch(url, { method, body, ...options });
+            const json = await resp.json();
+            console.log(e);
+            sendResponse(json);
+          } catch (e) {
+            console.log(e);
+            sendResponse({ error: true, e });
+          }
+          break;
+        default:
+          sendResponse({ error: true });
+          break;
+      }
+      return;
+    }
+    sendResponse({ error: true });
+  }
+);
